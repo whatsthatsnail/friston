6cf71dee5230059650c0bab5e334f439838516a8
Alexander Vail <alexv3931022@gmail.com>
2020-01-22 12:07:47 -0800

    Added ability to read in source files with os arguments

--- main.go
+++ main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt";
+	"os";
+	"io/ioutil";
 	"github.com/whatsthatsnail/simple_interpreter/lexer"
 )
 
+// Gets arguments when using 'go run *.go -- ...'
 func main() {
-	//scanner := lexer.NewLexer("+-(){},.;*===!!=<<=>>=")
-	scanner := lexer.NewLexer(fmt.Sprintf("// this is a comment \n {()} // grouping \n !*/+-=<> <= == // operators \n \"test 2\" 5.5 876. \n and class else false for func if or this true var while x y test"))
-	test, err := scanner.ScanTokens()
-	if !err || err {
-		lexer.PrintTokens(test)
+	args := os.Args[2:]
+
+	if len(args) >= 1 && args[0] == "repl" {
+		repl()
+	} else if len(args) >= 2 && args[0] == "file" {
+		file(args[1])
+	} else {
+		repl()
 	}
 }
 
+// Helper function to check for errors when reading files
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// TODO: Implement a REPL
+func repl() {
+	fmt.Println("Entering REPL:")
+}
+
+// Reads file into lexer, tokenizes, and prints tokens
+func file(path string) {
+	dat, err := ioutil.ReadFile(path)
+	check(err)
+
+	fmt.Println(path + ":" + "\n")
+	fmt.Println(string(dat) + "\n")
+
+	scanner := lexer.NewLexer(string(dat))
+	tokens, errflag := scanner.ScanTokens()
+	
+	if !errflag || errflag{
+		lexer.PrintTokens(tokens)
+	}
+}
