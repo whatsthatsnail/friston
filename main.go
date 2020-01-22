3f3ee3a3f275428de3f009abea07edb470e59038
abv <alexv3931022@gmail.com>
2020-01-22 21:31:30 +0000

    Added quiet flag.

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 	if len(args) >= 1 && args[0] == "repl" {
 		repl()
 	} else if len(args) >= 2 && args[0] == "file" {
-		file(args[1])
+		file(args[1], false)
+	} else if len(args) >= 3 && args[0] == "file" && args[2] == "-q" {
+		file(args[1], true)
 	} else {
 		repl()
 	}
@@ -33,17 +35,19 @@ func repl() {
 }
 
 // Reads file into lexer, tokenizes, and prints tokens
-func file(path string) {
+func file(path string, quiet bool) {
 	dat, err := ioutil.ReadFile(path)
 	check(err)
 
 	fmt.Println(path + ":" + "\n")
-	fmt.Println(string(dat) + "\n")
+	if !quiet {
+		fmt.Println(string(dat) + "\n")
+	}
 
 	scanner := lexer.NewLexer(string(dat))
 	tokens, errflag := scanner.ScanTokens()
-	
-	if !errflag || errflag{
+
+	if !errflag{
 		lexer.PrintTokens(tokens)
 	}
 }
